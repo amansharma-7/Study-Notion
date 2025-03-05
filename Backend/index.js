62fd25a21e1e332c43eb7d6a9baa355352d6ab51
amansharma-7 <amansharma40732@gmail.com>
2025-03-05 17:07:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://study-notion-by-aman.netlify.app/",
-    credentials: true,
+    origin: "https://study-notion-by-aman.netlify.app", 
+    methods: "GET,POST,PUT,DELETE",    credentials: true,
   })
 );
 
